05c5b89d4b3df4e45ac138b286587c3268151f3d
Jan Henckens <jan@onedge.be>
2014-11-08 12:05:07 +0100

    Added Gridset V1.0

--- header.php
+++ header.php
@@ -14,7 +14,6 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href="https://get.gridsetapp.com/31357/" rel="stylesheet" />
 
 <?php wp_head(); ?>
 </head>
