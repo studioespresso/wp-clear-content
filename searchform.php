730ccdeee8342afb0f55630e53e6968644482aca
Jan Henckens <jan@onedge.be>
2014-11-20 22:51:49 +0100

    Genericons + search field

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
 <div class="site-search">
 	<form id="main-search" action="/" method="get">
 		<fieldset>
-			<input type="search" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search" />
+			<input type="search" class="search-field" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search" />
 			<input type="hidden" value="post" name="post_type" id="post_type" />
-	<!-- 		<input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/images/search.png" />
-	 -->	</fieldset>
+			<button type="submit" value="" id="search-submit" name="submit" class="submit">
+		</fieldset>
 	</form>
-</div>
+</div><!-- .searchform -->
