e817cd973f1dbd76ddfc9cc3659850e79e5cc81d
Jan Henckens <jan@onedge.be>
2015-04-27 19:03:40 +0200

    Text-domain fix

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <div class="site-search">
 	<form id="main-search" action="/" method="get">
 		<fieldset>
-			<input type="search" class="search-field" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search" />
+        <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'clean-content' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 			<input type="hidden" value="post" name="post_type" id="post_type" />
 			<button type="submit" value="" id="search-submit" name="submit" class="submit">
 		</fieldset>
